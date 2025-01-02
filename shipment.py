bc64c4b7512dda34618300a145d5be93996688ec
Àngel Àlvarez <angel@nan-tic.com>
2025-01-02 11:57:46 +0100

    add recursive package

--- shipment.py
+++ shipment.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from trytond.model import ModelView, fields
 from trytond.pool import Pool, PoolMeta
 
@@ -12,6 +14,14 @@ class ShipmentOut(metaclass=PoolMeta):
     scrap_lines = fields.One2Many('scrap.line', 'shipment', 'Scrap Lines')
     related_scrap_lines = fields.One2Many('scrap.shipment',
            'shipment', 'Related Scrap Lines')
+    scrap_amount = fields.Function(fields.Numeric('Scrap Amount'),
+        'get_scrap_amount')
+
+    def get_scrap_amount(self, name):
+        amount = 0
+        for scrap in self.related_scrap_lines:
+            amount += scrap.weight * float(scrap.cost_price)
+        return Decimal(amount).quantize(Decimal('0.0001'))
 
     @classmethod
     @ModelView.button
@@ -21,6 +31,7 @@ class ShipmentOut(metaclass=PoolMeta):
         the shipment. Deletes existing scrap lines and creates new ones
         based on the outgoing moves.
         """
+
         pool = Pool()
         ScrapLine = pool.get('scrap.line')
         scrap_lines = []
@@ -32,8 +43,8 @@ class ShipmentOut(metaclass=PoolMeta):
                 scrap_lines += move.get_scrap_lines()
 
         ScrapLine.delete(to_delete)
-        ScrapLine.save(scrap_lines)
-
+        for l in scrap_lines:
+            l.save()
 
 class StockMove(metaclass=PoolMeta):
     """
@@ -48,22 +59,11 @@ class StockMove(metaclass=PoolMeta):
         Generates scrap lines based on the product's scrap template lines
         and the move's quantity.
         """
-
-        pool = Pool()
-        ScrapLine = pool.get('scrap.line')
-
         scrap_lines = []
         for sline in self.product.template.scrap_template_lines:
-            template = sline.product.template
-            scrap_line = ScrapLine()
-            scrap_line.category = template.scrap_category
-            scrap_line.product = sline.product
-            scrap_line.quantity = round(sline.get_quantity() * self.quantity, 4)
-            scrap_line.weight = round(sline.get_weight() * self.quantity, 4)
-            scrap_line.move = self
-            scrap_line.shipment = self.shipment
-            scrap_line.party = template.scrap_category.party
-            scrap_line.cost_price = template.scrap_category.cost_price
-            scrap_lines.append(scrap_line)
+            lines = sline._get_scrap_line(self.quantity)
+            for line in lines:
+                line.shipment = self.shipment
+                line.move = self
+            scrap_lines += lines
         return scrap_lines
-
